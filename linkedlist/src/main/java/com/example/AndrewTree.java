3cc1c73e76931bc13c4dba763e719d6d4fa3449f
Andrew Lee <andrew.ii.lee@gmail.com>
2018-04-04 15:48:22 -0500

    balanced tree AVL

--- linkedlist/src/main/java/com/example/AndrewTree.java
+++ linkedlist/src/main/java/com/example/AndrewTree.java
@@ -6,24 +6,29 @@ import java.util.List;
 public class AndrewTree {
 
     private Node tree;
+    private boolean preBalance;
 
-    private class Node {
+    AndrewTree(boolean preBalance) {
+        this.preBalance = preBalance;
+    }
+
+    class Node {
         int num;
         Node left;
         Node right;
-        char side;
+        int height;
 
-        Node(int num, char side) {
+        Node(int num) {
             this.num = num;
-            this.side = side;
+            height = 1;
         }
 
-        private void print() {
+        void print() {
             printTree("", true);
         }
 
-        private void printTree(String prefix, boolean isTail) {
-            System.out.println(prefix + (isTail ? "╰── " : "|── ") + num + side);
+        void printTree(String prefix, boolean isTail) {
+            System.out.println(prefix + (isTail ? "╰── " : "|── ") + num + ":" + height);
             List<Node> list = new ArrayList<>();
             if (right != null)
                 list.add(right);
@@ -42,27 +47,90 @@ public class AndrewTree {
         }
     }
 
-    public void insert(int num) {
-        tree = insert(tree, num, 'm');
+    void insert(int num) {
+        tree = insert(tree, num);
     }
 
-    private Node insert(Node node, int num, char side) {
+    Node insert(Node node, int num) {
         if (node == null)
-            return new Node(num, side);
+            return new Node(num);
 
         if (num > node.num) {
-            node.right = insert(node.right, num, 'r');
+            node.right = insert(node.right, num);
         } else if (num < node.num) {
-            node.left = insert(node.left, num, 'l');
+            node.left = insert(node.left, num);
         } else {
             System.out.println("duplicate num found");
             return null;
         }
 
+        node.height = 1 + max(height(node.left), height(node.right));
+
+        if (preBalance)
+            return node;
+
+        int balance = getBalance(node);
+
+        if (balance > 1 && num < node.left.num)
+            return rightRotate(node);
+
+        if (balance < -1 && num > node.right.num)
+            return leftRotate(node);
+
+        if (balance > 1 && num > node.left.num) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        if (balance < -1 && num < node.right.num) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+
         return node;
     }
 
-    public void print() {
+    int height(Node node) {
+        if (node == null)
+            return 0;
+        return node.height;
+    }
+
+    int max(int a, int b) {
+        return (a > b ? a : b);
+    }
+
+    int getBalance(Node node) {
+        if (node == null)
+            return 0;
+        return height(node.left) - height(node.right);
+    }
+
+    Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+
+        x.right = y;
+        y.left = T2;
+
+        x.height = max(height(x.left), height(x.right)) + 1;
+        y.height = max(height(y.left), height(y.right)) + 1;
+        return x;
+    }
+
+    Node leftRotate(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        x.height = max(height(x.left), height(x.right)) + 1;
+        y.height = max(height(y.left), height(y.right)) + 1;
+        return y;
+    }
+
+    void print() {
         tree.print();
     }
 }
--- linkedlist/src/test/java/com/example/TreeTest.java
+++ linkedlist/src/test/java/com/example/TreeTest.java
@@ -6,16 +6,35 @@ public class TreeTest {
 
     @Test
     void binary_tree_test() {
+        System.out.println("==== not balanced ====");
+        create_binary_tree(true).print();
+        System.out.println("==== balanced ====");
+        create_binary_tree(false).print();
 
-        AndrewTree tree = new AndrewTree();
+        System.out.println("==== not balanced ====");
+        create_second_tree(true).print();
+        System.out.println("==== balanced ====");
+        create_second_tree(false).print();
+    }
 
+    AndrewTree create_binary_tree(boolean value) {
+        AndrewTree testTree = new AndrewTree(value);
+        testTree.insert(10);
+        testTree.insert(20);
+        testTree.insert(30);
+
+        return testTree;
+    }
+
+    AndrewTree create_second_tree(boolean value) {
+        AndrewTree tree = new AndrewTree(value);
         tree.insert(50);
         tree.insert(100);
         tree.insert(25);
         tree.insert(30);
-        tree.insert(10);
-        tree.insert(200);
+        tree.insert(300);
+        tree.insert(400);
         tree.insert(500);
-        tree.print();
+        return tree;
     }
 }
